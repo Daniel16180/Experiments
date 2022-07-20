c73ba9d30da9d582d091e5dc79ed4d3806e98cef
Daniel Alegria <daniel.alegria@neoris.com>
2022-07-20 14:00:03 +0200

    Added simplest method possible: Text

--- LinQLearning/Program.cs
+++ LinQLearning/Program.cs
@@ -6,11 +6,18 @@ namespace LinQLearning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First method");
-            LazyDog lz = new LazyDog();
-            lz.MethodSyntax();
-            Console.WriteLine("Second method");
-            lz.ExpressionSyntax();
+            //DOG
+            //Console.WriteLine("First method");
+            //LazyDog lz = new LazyDog();
+            //lz.MethodSyntax();
+            //Console.WriteLine("Second method");
+            //lz.ExpressionSyntax();
+
+            //TEXT
+            string changingText = "A sorting operation orders the elements of a sequence based on one or more attributes. The first sort criterion performs a primary sort on the elements. By specifying a second sort criterion, you can sort the elements within each primary sort group.";
+            Text t = new Text();
+            // t.Test1(changingText);
+            t.Test2(changingText);
         }
     }
 }
---
+++ LinQLearning/Text.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinQLearning
+{
+    class Text
+    {
+        public void Test1(string text) {
+            //DATA SOURCE
+            string[] words = text.Split(" ");
+
+            //QUERY
+            var query = words
+                .OrderBy(or => or.Length);
+
+            //EXECUTION
+            foreach (var word in query)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public void Test2(string text) {
+            string[] words = text.Split(" ");
+
+            foreach (var word in words.OrderBy(or => or.Length))
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
